8b6dfbcdb1210255bb8b9217b0505c952c7435bf
Jonathan Blanchet <contact@jblanche.fr>
2015-03-03 19:09:04 +0100

    fix header everywhere

--- public/javascript/lib/scroll.js
+++ public/javascript/lib/scroll.js
@@ -10,28 +10,28 @@ var throttledUpdate = throttle(updatePosition, 100);
 var previousPosition = 0;
 
 function updatePosition () {
-  var $body = $body || $('body');
+  var $document = $document || $(document);
   var $topNotice = $topNotice || $('.notice.top');
 
-  if($body.scrollTop() > floatingLimit && previousPosition < floatingLimit){
+  if($document.scrollTop() > floatingLimit && previousPosition < floatingLimit){
     PubSub.publish('scoll:floatingLine:down');
   }
 
-  if($body.scrollTop() < floatingLimit && previousPosition > floatingLimit){
+  if($document.scrollTop() < floatingLimit && previousPosition > floatingLimit){
     PubSub.publish('scoll:floatingLine:up');
   }
 
   if($topNotice.length) {
-    if($body.scrollTop() > $topNotice.height() && previousPosition < $topNotice.height()) {
+    if($document.scrollTop() > $topNotice.height() && previousPosition < $topNotice.height()) {
       PubSub.publish('scoll:notice:down');
     }
 
-    if($body.scrollTop() < $topNotice.height() && previousPosition > $topNotice.height()) {
+    if($document.scrollTop() < $topNotice.height() && previousPosition > $topNotice.height()) {
       PubSub.publish('scoll:notice:up');
     }
   }
 
-  previousPosition = $body.scrollTop();
+  previousPosition = $document.scrollTop();
 }
 
 $(function () {
