f653ffc86dc7b9213e29d9be32d142428918ca91
Jonathan Blanchet <contact@jblanche.fr>
2015-03-05 16:50:33 +0100

    fix html in components

--- public/javascript/lsg.js
+++ public/javascript/lsg.js
@@ -48,8 +48,7 @@ lsg.highlight = function () {
     var $module = $(module);
     var node = tpl.clone();
     var code = $.trim($module.html());
-    node.find('code').text(html_beautify(code, {
-    }));
+    node.find('code').text(html_beautify(code));
     $module.next('.language-jade').after(node);
   });
 
