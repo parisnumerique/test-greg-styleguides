29ccca43a660e7790f33026236e232ee676edb58
Hans Lemuet <hans@etaminstudio.com>
2015-03-10 06:44:15 +0100

    Refactoring jecoute

--- src/modules/jecoute/script.js
+++ src/modules/jecoute/script.js
@@ -25,10 +25,10 @@ Paris.jecoute = (function(){
     }
 
     function changeVisibleItem() {
-      var current = $el.find('.visible') || $el.find('.item:first-child');
-      var $next = $el.find('.visible').next('.item');
+      var current = $el.find('.visible') || $el.find('.jecoute-item:first-child');
+      var $next = $el.find('.visible').next('.jecoute-item');
 
-      $el.find('.item').removeClass('answered');
+      $el.find('.jecoute-item').removeClass('answered');
 
       current.removeClass('visible');
 
@@ -36,30 +36,30 @@ Paris.jecoute = (function(){
         $next.addClass('visible');
       }
       else {
-        $next = $el.find('.item:first-child').addClass('visible');
+        $next = $el.find('.jecoute-item:first-child').addClass('visible');
       }
 
-      $next.find('.avancement').velocity({
+      $next.find('.progress').velocity({
           width: "100%"
       }, {
           duration: 10000,
           easing: 'linear',
           complete: function() {
-            $next.find('.question').velocity({scale: 1, translateY: 0}, {duration: 0 });
-            $next.find('.answer').velocity({scale: 0.5, translateY: '130px'}, {duration: 0 });
-            $next.find('.avancement').velocity({width: 0}, {duration: 0 });
+            $next.find('.jecoute-question').velocity({scale: 1, translateY: 0}, {duration: 0 });
+            $next.find('.jecoute-answer').velocity({scale: 0.5, translateY: '130px'}, {duration: 0 });
+            $next.find('.progress').velocity({width: 0}, {duration: 0 });
           }
       });
 
       setTimeout(function () {
         $next.addClass('answered');
 
-        $next.find('.question').velocity(
+        $next.find('.jecoute-question').velocity(
             {scale: 0.5, translateY: "-260px"},
             {
               duration: 300,
               complete: function () {
-                $next.find('.answer').velocity(
+                $next.find('.jecoute-answer').velocity(
                   {translateY: "-130px", scale: 1},
                   {duration: 300}
                 );
