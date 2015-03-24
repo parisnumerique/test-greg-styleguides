6bc055a7bce142c2e4db704cd8fcb1b60cdd2a6c
Hans Lemuet <hans@etaminstudio.com>
2015-03-24 16:34:25 +0100

    Add config and locales to gulp build task

--- gulp/tasks/paris.js
+++ gulp/tasks/paris.js
@@ -3,7 +3,7 @@ var clean      = require('gulp-clean');
 
 gulp.task('default', ['harp:serve', 'watch:js']);
 gulp.task('compile', ['harp:compile', 'compile:js']);
-gulp.task('build',   ['build:js', 'build:images', 'build:fonts', 'build:css', 'build:themes', 'build:jade'], function () {
+gulp.task('build',   ['build:js', 'build:images', 'build:fonts', 'build:css', 'build:themes', 'build:jade', 'copy:config', 'copy:locales'], function () {
     return gulp.src('src/stylesheets/paris-*.scss', {read: false})
         .pipe(clean());
 });
