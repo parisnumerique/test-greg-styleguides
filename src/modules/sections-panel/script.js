bbaaca568db79dc8b4819233ceaff2bbecfc2dbb
Hans Lemuet <hans@etaminstudio.com>
2015-04-12 21:26:24 +0200

    Improved sections-panel + hub/subnav-open

--- src/modules/sections-panel/script.js
+++ src/modules/sections-panel/script.js
@@ -1,5 +1,7 @@
 'use strict';
 require('velocity-animate');
+var has = require('lodash.has');
+var values = require('lodash.values');
 
 var Paris = window.Paris || {};
 
@@ -17,10 +19,11 @@ Paris.sectionsPanel = (function(){
     var $el     = $(selector),
       options = $.extend({}, defaultOptions, userOptions),
       $nav, $subnav, $content,
-      $navItems, $navItemsLinks, $navOverlay,
+      $navItems, $navItemsLinks, $navMore,
       $subnavSections, $subnavSectionsLinks, $subnavDefault,
       $contentWrapper,
-      currentLevel = "nav";
+      currentLevel = "nav",
+      heights = {};
 
     function init(){
       initOptions();
@@ -28,6 +31,7 @@ Paris.sectionsPanel = (function(){
       $nav = $el.find('.sections-panel-nav');
       $navItems = $nav.find('.sections-panel-nav-items');
       $navItemsLinks = $navItems.find('a');
+      $navMore = $nav.find('.sections-panel-nav-more');
 
       $subnav = $el.find('.sections-panel-subnav');
       $subnavDefault = $subnav.find('.sections-panel-subnav-default');
@@ -37,15 +41,10 @@ Paris.sectionsPanel = (function(){
       $content = $el.find('.sections-panel-content');
       $contentWrapper = $content.find('.sections-panel-content-wrapper');
 
-      $navOverlay = $('<div class="sections-panel-nav-overlay" />').appendTo($nav);
+      setHeight();
 
-      //$el.on('mouseenter', onMouseEnter);
-      //$el.on('mouseleave', onMouseLeave);
       $navItemsLinks.on('click', onClickNavLink);
-      $nav.on('mouseenter', onMouseEnterNav);
-      $nav.on('mouseleave', onMouseLeaveNav);
       $subnavSectionsLinks.on('click', onClickSubnavLink);
-
     }
 
     function initOptions() {
@@ -53,48 +52,52 @@ Paris.sectionsPanel = (function(){
         options[key] = value;
       });
     }
-    //
-    //function onMouseEnter(e){
-    //  console.log('onMouseEnter', e);
-    //  if (currentLevel !== "content") {return;}
-    //  $subnav.velocity({
-    //    translateX: -190
-    //  }, options.velocity);
-    //}
 
-    //function onMouseLeave(e){
-    //  console.log('onMouseLeave', e);
-    //  if (currentLevel !== "content") {return;}
-    //  if (currentLevel !== "content") {return;}
-    //  $subnav.velocity({
-    //    translateX: -200
-    //  }, options.velocity);
-    //}
-    //
-    function onMouseEnterNav(){
-      //console.log('onMouseEnterNav', currentLevel);
-      if (currentLevel !== "content") {return;}
-      $navOverlay.velocity({
-        opacity: 0
-      }, $.extend({}, options.velocity, {display: 'none'}));
+    function setHeight() {
+      var newHeight = calculateHeight();
+      $el.css("height", newHeight);
+      //$el.velocity({
+      //  height: newHeight
+      //}, options.velocity);
+      //console.log("== setHeight", newHeight);
     }
 
-    function onMouseLeaveNav(){
-      //console.log('onMouseLeaveNav', currentLevel);
-      if (currentLevel !== "content") {return;}
-      $navOverlay.velocity({
-        opacity: 1
-      }, $.extend({}, options.velocity, {display: 'block'}));
+    function calculateHeight(){
+      if (!has(heights, 'nav')) {
+        var navHeight = $navItems.outerHeight(true) + $navMore.outerHeight();
+        var navPadding = $nav.outerHeight() - $nav.height();
+        //console.log("navHeight", navHeight);
+        //console.log("navPadding", navPadding);
+        heights.nav = navHeight + navPadding;
+      }
+      if (!has(heights, 'subNav')) {
+        var subNavHeight = 0;
+        var subNavPadding = $subnav.outerHeight() - $subnav.height();
+        $subnavSections.add($subnavDefault).each(function () {
+          console.log($(this).attr('id'), $(this).innerHeight(), $(this).height(), $(this).outerHeight());
+          subNavHeight = Math.max(subNavHeight, $(this).outerHeight());
+        });
+        //console.log("subNavHeight", subNavHeight);
+        //console.log("subNavPadding", subNavPadding);
+        heights.subNav = subNavHeight + subNavPadding;
+      }
+      var contentHeight = $contentWrapper.outerHeight();
+      var contentPadding = $content.outerHeight() - $content.height();
+      console.log("contentHeight", contentHeight);
+      console.log("contentPadding", contentPadding);
+      heights.content = contentHeight + contentPadding;
+      console.log("heights", heights);
+      return Math.max.apply(null, values(heights));
     }
 
     function onClickNavLink(e) {
       e.preventDefault();
       var $this = $(this);
       var subnavSection = $this.data('subnav-section');
-      $navItemsLinks.removeClass("active");
-      $this.addClass("active");
+      $navItemsLinks.removeClass("current");
+      $this.addClass("current");
+      if (currentLevel === "content") {closeContent();}
       openSubnavSection(subnavSection);
-      closeContent();
     }
 
     function openSubnavSection(section) {
@@ -111,43 +114,50 @@ Paris.sectionsPanel = (function(){
       e.preventDefault();
       var $this = $(this);
       var url = $this.attr('href');
-      $subnavSectionsLinks.removeClass("active");
-      $this.addClass("active");
+      $subnavSectionsLinks.removeClass("current");
+      $this.addClass("current");
       openContent(url);
     }
 
     function openContent(url){
-      $contentWrapper.load(url).velocity({
-        opacity: 1
-      }, $.extend({}, options.velocity, {display: 'block'}));
-      if (currentLevel !== "content") {
-        $subnav.velocity({
-          translateX: -200
-        }, options.velocity);
-        $navOverlay.velocity({
+      $contentWrapper.load(url, function(){
+        setHeight();
+        $(this).velocity({
           opacity: 1
         }, $.extend({}, options.velocity, {display: 'block'}));
+      });
+      if (currentLevel !== "content") {
+        $nav.addClass("closed");
+        $subnav.velocity({
+          translateX: -200
+        });
         $content.velocity({
           opacity: 1,
-          translateX: [-200, 100]
-        }, $.extend({}, options.velocity, {display: 'block'}));
+          translateX: [0, 200]
+        }, $.extend({}, options.velocity, {
+          delay: 200,
+          display: 'block'
+        }));
       }
       currentLevel = "content";
     }
 
     function closeContent(){
-      $subnavSectionsLinks.removeClass("active");
+      $nav.removeClass("closed");
+      $subnavSectionsLinks.removeClass("current");
       $contentWrapper.empty();
       $subnav.velocity({
         translateX: 0
-      }, options.velocity);
-      $navOverlay.velocity({
-        opacity: 0
-      }, $.extend({}, options.velocity, {display: 'none'}));
+      }, $.extend({}, options.velocity, {
+        delay: 200
+      }));
       $content.velocity({
         opacity: 0,
-        translateX: [100, -200]
-      }, $.extend({}, options.velocity, {display: 'none'}));
+        translateX: [200, 0]
+      }, $.extend({}, options.velocity, {
+        complete: setHeight,
+        display: 'none'
+      }));
       currentLevel = "subnav";
     }
 
