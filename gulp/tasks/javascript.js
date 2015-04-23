cfe3b8bbedeb00b04c281c2951511eaeb549af4d
Jonathan Blanchet <contact@jblanche.fr>
2015-04-23 18:19:31 +0200

    update templatizer tasks

--- gulp/tasks/javascript.js
+++ gulp/tasks/javascript.js
@@ -7,7 +7,9 @@ var path       = require('path');
 var source     = require('vinyl-source-stream');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify     = require('gulp-uglify');
+var insert     = require('gulp-insert');
 var watchify   = require('watchify');
+var fs         = require('fs');
 
 var browserifyBundler = browserify('./src/javascript/main.js', watchify.args);
 var watchifyBundler   = watchify(browserify('./src/javascript/main.js', watchify.args));
@@ -15,8 +17,8 @@ var watchifyBundler   = watchify(browserify('./src/javascript/main.js', watchify
 browserifyBundler.transform('jadeify');
 watchifyBundler.transform('jadeify');
 
-gulp.task('watch:js', watch); // so you can run `watch:js` to build the file
-gulp.task('compile:js', compile); // so you can run `compile:js` to build the file
+gulp.task('watch:js', ['build:clients'],  watch); // so you can run `watch:js` to build the file
+gulp.task('compile:js', ['build:clients'], compile); // so you can run `compile:js` to build the file
 gulp.task('build:js', build); // so you can run `gulp build:js` to build the file
 gulp.task('copy:config', copyConfig);
 gulp.task('copy:locales', copyLocales);
@@ -25,12 +27,13 @@ watchifyBundler.on('update', watch); // on any dep update, runs the watchifyBund
 watchifyBundler.on('log', gutil.log); // output build logs to terminal
 
 function watch() {
-  return watchifyBundler.bundle()
+  watchifyBundler.bundle()
     .on('error', gutil.log.bind(gutil, 'Watchify Error'))
     .pipe(source(config.js.output))
-      .pipe(buffer())
-      .pipe(sourcemaps.init({loadMaps: true}))
-      .pipe(sourcemaps.write('./'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('./'))
+    .pipe(insert.prepend(fs.readFileSync(config.build.assets.javascript + '/client.tpl.js')))
     .pipe(gulp.dest(path.join(config.harp.input, 'javascript')));
 }
 
@@ -49,6 +52,7 @@ function bundle(output) {
     .pipe(source(config.js.output))
     .pipe(buffer())
     .pipe(uglify())
+    .pipe(insert.prepend(fs.readFileSync(config.build.assets.javascript + '/client.tpl.js')))
     .pipe(gulp.dest(output)) ;
 }
 
