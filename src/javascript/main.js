ae4a83fbd4130daa6cfdca57e803707bb57ce40a
Hans Lemuet <hans@etaminstudio.com>
2015-05-18 17:33:41 +0200

    Added form handling script for block-content-newsletter

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -18,6 +18,7 @@ require('../layouts/left-col/script');
 // load our modules
 require('../modules/anchors-list/script');
 require('../modules/block-content-jecoute/script');
+require('../modules/block-content-newsletter/script');
 require('../modules/button-top/script');
 require('../modules/document-heading/script');
 require('../modules/gallery-ugc/script');
