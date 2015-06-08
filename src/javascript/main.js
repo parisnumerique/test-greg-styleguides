3240b19ac1fb65e149576370ed680a0f748d188e
Hans Lemuet <hans@etaminstudio.com>
2015-06-08 11:23:50 +0200

    New build, bump version number to 0.12.1

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -36,5 +36,5 @@ require('../templates/list-persons/script');
 require('../templates/search/script');
 
 var Paris = window.Paris || {};
-Paris.version = "0.12.0";
+Paris.version = "0.12.1";
 Paris.templates = {};
