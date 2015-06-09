e219ac89b6257313ae28e9f75a669eb54a712f04
Hans Lemuet <hans@etaminstudio.com>
2015-06-09 18:49:49 +0200

    Rename CNIL cookie

--- src/javascript/config.js
+++ src/javascript/config.js
@@ -16,8 +16,8 @@ Paris.config = {
   },
   cnil: {
     cookie: {
-      name: "CNIL",
-      value: "Accepted",
+      name: "cookies",
+      value: "accepted",
       // A number (of seconds), a date parsable string, or a Date object of when the cookie will expire
       expires: 34186670 // 13 months
     }
