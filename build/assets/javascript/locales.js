b565182f12969f7822012377c361fde965664b61
Hans Lemuet <hans@etaminstudio.com>
2015-06-22 15:02:41 +0200

    New build, bump version number to 0.13.5

--- build/assets/javascript/locales.js
+++ build/assets/javascript/locales.js
@@ -2,85 +2,98 @@ var Paris = window.Paris || {};
 Paris.i18n = Paris.i18n || {};
 
 Paris.i18n.locales = {
-  fr: {
-    share: {
-      facebook: 'Partager sur Facebook',
-      twitter: 'Partager sur Twitter',
-      mail: 'Partager par email'
+  "fr": {
+    "share": {
+      "facebook": "Partager sur Facebook",
+      "twitter": "Partager sur Twitter",
+      "mail": "Partager par email"
     },
-    search_results: {
-      title: {
-        one: 'Un résultat',
-        plural: '{0} résultats'
+    "search_results": {
+      "search": "Recherche",
+      "title": {
+        "one": "Un résultat",
+        "plural": "{0} résultats"
       },
-      page: "Page {0}",
-      no_result: "Désolé, votre recherche ne correspond à aucun de nos contenus.",
-      more: "Afficher plus de résultats",
-      facets: {
-        onglet: "Catégories",
+      "page": "Page {0}",
+      "no_result": "Désolé, votre recherche ne correspond à aucun de nos contenus.",
+      "more": "Afficher plus de résultats",
+      "facets": {
+        "onglet": "Catégories",
         "groupe_politique": "Groupe politique",
         "secteur": "Arrondissement"
       }
     },
-    list_persons: {
-      no_result: "Personne ne correspond à votre recherche.",
+    "list_persons": {
+      "no_result": "Personne ne correspond à votre recherche."
     },
-    person_block: {
-      button_view: "Voir la fiche"
+    "person_block": {
+      "button_view": "Voir la fiche"
+    },
+    "pagination": {
+      "prev": "Page précédente",
+      "next": "Page suivante"
     }
   },
-  en: {
-    share: {
-      facebook: 'Share on Facebook',
-      twitter: 'Share on Twitter',
-      mail: 'Share by email'
+  "en": {
+    "share": {
+      "facebook": "Share on Facebook",
+      "twitter": "Share on Twitter",
+      "mail": "Share by email"
     },
-    search_results: {
-      title: {
-        one: 'One result',
-        plural: '{0} results'
+    "search_results": {
+      "title": {
+        "one": "One result",
+        "plural": "{0} results"
       },
-      page: "Page {0}",
-      no_result: "Sorry, your search did not match any of our contents.",
-      more: "See more results",
-      facets: {
-        onglet: "Categories",
+      "page": "Page {0}",
+      "no_result": "Sorry, your search did not match any of our contents.",
+      "more": "See more results",
+      "facets": {
+        "onglet": "Categories",
         "groupe_politique": "Political group",
         "secteur": "District"
       }
     },
-    list_persons: {
-      no_result: "No people matched your search.",
+    "list_persons": {
+      "no_result": "No people matched your search."
     },
-    person_block: {
-      button_view: "See details"
+    "person_block": {
+      "button_view": "See details"
+    },
+    "pagination": {
+      "prev": "Previous page",
+      "next": "Next page"
     }
   },
-  es: {
-    share: {
-      facebook: 'Compartir en Facebook',
-      twitter: 'Compartir en Twitter',
-      email: 'Compartir por correo electrónico'
+  "es": {
+    "share": {
+      "facebook": "Compartir en Facebook",
+      "twitter": "Compartir en Twitter",
+      "mail": "Compartir por correo electrónico"
     },
-    search_results: {
-      title: {
-        one: 'Un resultado',
-        plural: '{0} resultados'
+    "search_results": {
+      "title": {
+        "one": "Un resultado",
+        "plural": "{0} resultados"
       },
-      page: "Página {0}",
-      no_result: "Lo sentimos, su búsqueda no coincide con ninguno de nuestros contenidos.",
-      more: "Ver más resultados",
-      facets: {
-        onglet: "Categorías",
+      "page": "Página {0}",
+      "no_result": "Lo sentimos, su búsqueda no coincide con ninguno de nuestros contenidos.",
+      "more": "Ver más resultados",
+      "facets": {
+        "onglet": "Categorías",
         "groupe_politique": "Grupo político",
         "secteur": "Distrito"
       }
     },
-    list_persons: {
-      no_result: "Ningúna persona concuerda con su búsqueda.",
+    "list_persons": {
+      "no_result": "Ningúna persona concuerda con su búsqueda."
     },
-    person_block: {
-      button_view: "Ver detalles"
+    "person_block": {
+      "button_view": "Ver detalles"
+    },
+    "pagination": {
+      "prev": "página anterior",
+      "next": "página siguiente"
     }
   }
 };
