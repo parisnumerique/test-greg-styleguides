0911b00c265bcd8ec90fb656af3d4b9e0215165b
Hans Lemuet <hans@etaminstudio.com>
2015-07-22 23:00:10 +0200

    Forms validation in progress

--- src/components/form/script.js
+++ src/components/form/script.js
@@ -6,19 +6,49 @@ Paris.form = (function(){
 
   function form(selector){
     var $el = $(selector),
-      $captcha;
+      $captcha,
+      $currentField;
 
     function init(){
       $captcha = $el.find('.form-item-captcha');
 
-      if ($captcha.length) {
-        renderCaptcha();
+      $el.on('focus', 'input, textarea, select', onFieldFocus);
+      $el.on('blur', 'input, textarea, select', onFieldBlur);
+      $el.on('submit', onSubmit);
+
+      renderCaptcha();
+    }
+
+    function onFieldFocus(){
+      $currentField = $(this);
+      $currentField.on('input', onCurrentFieldInput)
+                   .on('change', onCurrentFieldInput);
+    }
+
+    function onCurrentFieldInput(){
+      if ($currentField) {
+        var validity = $currentField.get(0).validity;
+        var $formItem = $currentField.closest('.form-item, .matrix-item');
+        $formItem.toggleClass('valid', validity.valid).toggleClass('error', !validity.valid);
       }
     }
 
+    function onFieldBlur(){
+      onCurrentFieldInput();
+      $currentField.off('keypress', onCurrentFieldInput);
+      $currentField = null;
+    }
+
+    function onSubmit(){
+      console.log("form submitted", $el.get(0).validity);
+    }
+
+
     function renderCaptcha(){
-      $captcha.find('.g-recaptcha').attr('data-sitekey', Paris.config.captcha.key);
-      $('body').append('<script src="https://www.google.com/recaptcha/api.js" async defer></script>');
+      if ($captcha.length) {
+        $captcha.find('.g-recaptcha').attr('data-sitekey', Paris.config.captcha.key);
+        $('body').append('<script src="https://www.google.com/recaptcha/api.js" async defer></script>');
+      }
     }
 
     init();
