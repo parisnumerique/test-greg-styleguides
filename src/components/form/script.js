8f348dcc7d039272590ede36628f934a0daa28f1
Hans Lemuet <hans@etaminstudio.com>
2015-07-24 17:29:24 +0200

    Form validations ok (for matrix too)

--- src/components/form/script.js
+++ src/components/form/script.js
@@ -12,25 +12,70 @@ Paris.form = (function(){
     function init(){
       $captcha = $el.find('.form-item-captcha');
 
-      $el.on('focus', 'input, textarea, select', onFieldFocus);
-      $el.on('blur', 'input, textarea, select', onFieldBlur);
+      $el.on('focus', '.form-field', onFieldFocus);
+      $el.on('blur', '.form-field', onFieldBlur);
       $el.on('submit', onSubmit);
 
       renderCaptcha();
+
+      // Check fields validity
+      //$el.find('.form-field').each(function(){
+      //  checkValidity($(this));
+      //});
     }
 
     function onFieldFocus(){
       $currentField = $(this);
-      $currentField.on('input', onCurrentFieldInput)
-                   .on('change', onCurrentFieldInput);
+
+      switch ($currentField.attr('type') || $currentField.prop('tagName').toLowerCase()){
+        case 'checkbox':
+        case 'radio':
+        case 'select':
+          var event = 'change';
+          break;
+
+        default:
+          var event = 'input';
+      }
+
+      $currentField.on(event, onCurrentFieldInput);
+    }
+
+    function checkValidity($field){
+      if ($field) {
+        var valid = isValid($field);
+        var $formItem = $field.closest('.form-item, .matrix-item');
+        $formItem.toggleClass('valid', valid).toggleClass('error', !valid);
+      }
+    }
+
+    function isValid($field){
+      if ($field) {
+        var validity = false;
+
+        if ($field.attr('type') === "checkbox") {
+          var $formItem = $field.closest('.form-item');
+          if (!$formItem.hasClass("required")) {return true;}
+          var $matrixItem = $field.closest('.matrix-item');
+          if ($matrixItem.length) {
+            var $checkboxes = $matrixItem.find('input[type="checkbox"]');
+          } else {
+            var $checkboxes = $formItem.find('input[type="checkbox"]');
+          }
+          $checkboxes.each(function(){
+            if ($(this).is(':checked')) {validity = true;}
+          });
+          return validity;
+        } else {
+          validity = $field.get(0).validity;
+          return validity.valid;
+        }
+
+      }
     }
 
     function onCurrentFieldInput(){
-      if ($currentField) {
-        var validity = $currentField.get(0).validity;
-        var $formItem = $currentField.closest('.form-item, .matrix-item');
-        $formItem.toggleClass('valid', validity.valid).toggleClass('error', !validity.valid);
-      }
+      checkValidity($currentField);
     }
 
     function onFieldBlur(){
