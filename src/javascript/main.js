528ffff003bbf8d779b2e79e7c04fdf126b6a6d1
Hans Lemuet <hans@etaminstudio.com>
2015-11-06 11:59:35 +0100

    a11y list-persons, person-block

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -28,6 +28,7 @@ require('../modules/block-content-newsletter/script');
 require('../modules/button-top/script');
 require('../modules/gallery-ugc/script');
 require('../modules/notice/script');
+require('../modules/person-block/script');
 require('../modules/poll/script');
 require('../modules/quick-access/script');
 require('../modules/rheader/script');
